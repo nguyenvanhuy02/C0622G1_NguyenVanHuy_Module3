797b709ac40fe572a7c9dde39d395fce5e6da35e
nguyenvanhuy02 <maohuy123312@gmail.com>
2022-09-16 01:04:15 +0700

    nopbt

--- case_study/sqp_co_ban_11_15.sql
+++ case_study/sqp_co_ban_11_15.sql
@@ -55,8 +55,44 @@ left join hop_dong hd
 on hdct.ma_hop_dong = hd.ma_hop_dong
 );
 
+-- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất.
+-- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung
+-- (được tính dựa trên việc count các ma_dich_vu_di_kem).
+
+select hd.ma_hop_dong ,ldv.ten_loai_dich_vu,dvdk.ten_dich_vu_di_kem ,count(hdct.so_luong) as so_luong
+from dich_vu_di_kem dvdk
+left join hop_dong_chi_tiet hdct
+on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+left join hop_dong hd
+on hdct.ma_hop_dong = hd.ma_hop_dong
+join dich_vu dv
+on hd.ma_dich_vu = dv.ma_dich_vu
+join loai_dich_vu ldv
+on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+group by dvdk.ma_dich_vu_di_kem
+having so_luong = (
+select min(hdct.so_luong)
+from dich_vu_di_kem dvdk
+left join hop_dong_chi_tiet hdct
+on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
+left join hop_dong hd
+on hdct.ma_hop_dong = hd.ma_hop_dong
+)
+order by hd.ma_hop_dong;
 
 
+-- 15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, dia_chi
+-- mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
 
-
-
+select nv.ma_nhan_vien, nv.ho_va_ten, td.ten_trinh_do, bp.ten_bo_phan, nv.so_dien_thoai, nv.dia_chi
+from nhan_vien nv
+right join hop_dong hd
+on nv.ma_nhan_vien = hd.ma_nhan_vien
+join trinh_do td
+on nv.ma_trinh_do = td.ma_trinh_do
+join bo_phan bp
+on nv.ma_bo_phan = bp.ma_bo_phan
+where year(hd.ngay_lam_hop_dong) between 2020 and 2021
+group by nv.ma_nhan_vien
+having count(nv.ma_nhan_vien)<= 3
+order by nv.ma_nhan_vien;
